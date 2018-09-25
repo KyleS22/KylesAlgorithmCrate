30b5899bdf5ae27b372a6cbb54f829dc3c9f4363
KyleS22 <25314187+KyleS22@users.noreply.github.com>
2018-09-25 08:34:56 -0600

    Made some progress with trees, having trouble with Copy traits

--- kyles_algorithm_crate/src/tree.rs
+++ kyles_algorithm_crate/src/tree.rs
@@ -1,11 +1,17 @@
+
+
 pub struct Tree<T> {
     data: T,
-    parent: Box<Tree<T>>,
+    _parent: Option<Box<Tree<T>>>,
     children: Vec<Tree<T>>,
 }
 
 impl<T> Tree<T> 
     where T: Clone {
+
+    pub fn new(root_val: T) -> Self {
+        Tree { data: root_val, _parent: None, children: Vec::new()}
+    }
 
     pub fn postorder_traversal(self) -> Vec<T> {
         let mut values: Vec<T> = Vec::new();
@@ -39,6 +45,33 @@ impl<T> Tree<T>
 mod tests {
     #[test]
     fn test_postorder_traversal() {
-        assert!(false);
+        use tree::Tree;
+
+        let mut root = Tree::new(1);
+
+        let result = root.postorder_traversal();
+        
+        let mut expected = Vec::new();
+        expected.push(1);
+
+        assert_eq!(expected, result);
+
+        let two = Tree::new(2);
+        let three = Tree::new(3);
+        let four = Tree::new(4);
+
+        root.children.push(two);
+        root.children.push(three);
+        root.children.push(four);
+
+        let result = root.postorder_traversal();
+
+        let mut expected = Vec::new();
+        expected.push(2);
+        expected.push(3);
+        expected.push(4);
+        expected.push(1);
+
+        assert_eq!(expected, result);
     }
 }
