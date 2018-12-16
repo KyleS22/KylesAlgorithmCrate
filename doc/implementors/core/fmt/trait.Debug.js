75f6127be556b628e14a22d34d1ed2227e1f7f7b
KyleS22 <25314187+KyleS22@users.noreply.github.com>
2018-12-16 17:34:09 -0600

    Added documentation

---
+++ doc/implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["kyles_algorithm_crate"] = [{text:"impl&lt;T:&nbsp;<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"kyles_algorithm_crate/graph/struct.Node.html\" title=\"struct kyles_algorithm_crate::graph::Node\">Node</a>&lt;T&gt;",synthetic:false,types:["kyles_algorithm_crate::graph::Node"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"kyles_algorithm_crate/graph/struct.Edge.html\" title=\"struct kyles_algorithm_crate::graph::Edge\">Edge</a>",synthetic:false,types:["kyles_algorithm_crate::graph::Edge"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
